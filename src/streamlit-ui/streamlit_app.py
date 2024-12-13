751e5a38dc5bca0ef07d1d46bc3b94c3b6e97afc
shreyansh05s <shreyansh05s@gmail.com>
2024-12-13 17:20:35 +0100

    Fixed networking and connection

--- src/streamlit-ui/streamlit_app.py
+++ src/streamlit-ui/streamlit_app.py
@@ -2,7 +2,7 @@ import requests
 import streamlit as st
 
 # OpenSearch configuration
-opensearch_url = "http://opensearch:9200"
+opensearch_url = "https://opensearch:9200"
 
 # Label Studio configuration
 # label_studio_url = "http://label-studio:8080"
@@ -11,9 +11,12 @@ st.title("Streamlit Interface for OpenSearch")
 
 # Test OpenSearch Connection
 try:
-    response = requests.get(f"{opensearch_url}/_cluster/health")
-    st.write("OpenSearch Status:", response.json())
-except requests.ConnectionError:
+    response = requests.get(f"{opensearch_url}/_cluster/health", verify=False, auth=('admin', 'Duck1Teddy#Open'))
+    st.write("OpenSearch Status Code:", response.status_code)
+    st.write("OpenSearch Headers:", response.content)
+    # st.write("OpenSearch Status:", response.json())
+except requests.ConnectionError as e:
+    st.write(e)
     st.write("Error: Unable to connect to OpenSearch.")
 
 # Test Label Studio Connection
@@ -38,10 +41,11 @@ if uploaded_file is not None:
         # Replace 'your_api_url' with the actual API endpoint
         tagging_api_url = "http://tagging:8000/process_audio"
         # embedding_api_url = "http://your-api-service/upload"
-        
+
         # Send file to the API
         files = {"file": (uploaded_file.name, file_content)}
-        tagging_response = requests.post(tagging_api_url, files=files, data={"filename": uploaded_file.name})
+        # data = {"audio_name": uploaded_file.name}
+        tagging_response = requests.post(tagging_api_url, files=files)
         # embedding_response = requests.post(embedding_api_url, files=files, data={"filename": uploaded_file.name})
         
         if tagging_response.status_code == 200 and embedding_response == 200:
