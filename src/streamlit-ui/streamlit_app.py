b3ec02960dc55cd17c6ad7f4b6f9250501bb1708
vinnayakk <125215423+vinnayakk@users.noreply.github.com>
2024-12-15 01:11:34 +0100

    Update streamlit_app.py
    UI changes.

--- src/streamlit-ui/streamlit_app.py
+++ src/streamlit-ui/streamlit_app.py
@@ -1,5 +1,7 @@
 import requests
 import streamlit as st
+import pandas as pd
+import altair as alt
 
 # OpenSearch configuration
 opensearch_url = "https://opensearch:9200"
@@ -7,13 +9,13 @@ opensearch_url = "https://opensearch:9200"
 # Label Studio configuration
 # label_studio_url = "http://label-studio:8080"
 
-st.title("Streamlit Interface for OpenSearch")
+st.title("Zero-Shot Annotator")
 
 # Test OpenSearch Connection
 try:
     response = requests.get(f"{opensearch_url}/_cluster/health", verify=False, auth=('admin', 'Duck1Teddy#Open'))
-    st.write("OpenSearch Status Code:", response.status_code)
-    st.write("OpenSearch Headers:", response.content)
+    #st.write("OpenSearch Status Code:", response.status_code)
+    #st.write("OpenSearch Headers:", response.content)
     # st.write("OpenSearch Status:", response.json())
 except requests.ConnectionError as e:
     st.write(e)
@@ -27,7 +29,7 @@ except requests.ConnectionError as e:
 #     st.write("Error: Unable to connect to Label Studio.")
 
 # File upload section
-uploaded_file = st.file_uploader("Choose a file", type=["wav", "mp3"])
+uploaded_file = st.file_uploader("Choose a File (Min. Duration: 10 sec)", type=["wav", "mp3"])
 tagging_response = None
 embedding_response = None
 if uploaded_file is not None:
@@ -48,16 +50,46 @@ if uploaded_file is not None:
         tagging_response = requests.post(tagging_api_url, files=files)
         # embedding_response = requests.post(embedding_api_url, files=files, data={"filename": uploaded_file.name})
         
-        st.write("Tagging Status Code:", tagging_response.status_code)
+       # st.write("Tagging Status Code:", tagging_response.status_code)
         # st.write("Tagging response:", tagging_response.content)
         
         if tagging_response.status_code == 200:
-            st.write("File successfully Tagged.")
+            st.write("Your annotations are ready!")
             # show the tags
             labels = tagging_response.json()["labels"]	
             probs = tagging_response.json()["probabilities"]
-            st.write("Tags:", labels)
-            st.write("Probabilities:", probs)
+
+            # Filter probabilities to match the length of identified tags
+            relevant_probs = probs[:len(labels)]
+
+            #st.write("Annotations:", labels)
+            #st.write("Probabilities:", relevant_probs)
+
+            # Create a DataFrame
+            df = pd.DataFrame({"Annotation": labels, "Confidence Score": relevant_probs})
+
+            # Display the DataFrame
+            st.dataframe(df)
+
+            # Save DataFrame to CSV
+            csv = df.to_csv(index=False).encode('utf-8')
+            st.download_button(
+                label="Download Tags as CSV",
+                data=csv,
+                file_name=f"{uploaded_file.name}_tags.csv",
+                mime='text/csv'
+            )
+
+            # Visualization 
+            chart = alt.Chart(df).mark_bar().encode( 
+                x='Annotation', 
+                y='Confidence Score', 
+                color='Annotation', 
+                tooltip=['Annotation', 'Confidence Score'] 
+            ).properties( 
+                title='Annotation & Confidence Scores' 
+            ) 
+            st.altair_chart(chart, use_container_width=True)
             
         else:
             st.write("Error: Unable to tag the file.")
